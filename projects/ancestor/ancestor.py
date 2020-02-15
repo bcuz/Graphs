c19d634bcb30088b4582794a3b5d01f9f86d7272
adam <adam.nawroc@gmail.com>
2020-02-15 12:58:09 -0500

    adding data to a graph

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -4,23 +4,27 @@ from graph import Graph
 
 def earliest_ancestor(ancestors, starting_node):
   # seems like depth first
+  # planning just seems like procrastination, mayne
   # always go in the direction of the lower number when there's a fork in the road
   # something about when the last one is reached in the longest branch, return the last node and stop traversing
   # can just try and solve first, worry about proper structure after (helper fcs, so on)
 
   # 1 - 11
-  
-  x = []
+  graph = Graph()  
+  allNodes = []
 
+  # loop to add each vertex
   for tup in ancestors:
-    for vl in tup:
-      if vl not in x:
-        x.append(vl)
+    if tup[0] not in allNodes:
+      allNodes.append(tup[0])
+      graph.add_vertex(tup[0])
+    if tup[1] not in allNodes:
+      allNodes.append(tup[1])
+      graph.add_vertex(tup[1])    
 
+  # loop to add edges
+  for tup in ancestors:
+    graph.add_edge(tup[0], tup[1])
 
-  x.sort()
-  print(x)
-
-
-
-
+  # add both ways? or maybe add the edges in opposite.
+  print(graph.get_neighbors(3))
